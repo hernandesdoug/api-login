76947cd1c63fcb08e928650788e9477a4db67b0f
Douglas Hernandes <douglashernandes@icloud.com>
2026-08-03 20:21:44 -0300

    adicionar URL no swagger

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -10,7 +10,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3333"
+                url: process.env.API_URL || "http://localhost:3333"
             }
         ],
         components: {
