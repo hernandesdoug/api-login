6767e7ead2dbe0584c335cad2736221700981e7d
Douglas Hernandes <douglashernandes@icloud.com>
2026-08-03 15:10:44 -0300

    adicionar PORT no arquivo env

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 
 app.use(usersRoutes);
 
-app.listen(3333, () => {
-    console.log("Servidor executando na porta 3333");
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+    console.log(`Servidor executando na porta ${PORT}`);
 })
