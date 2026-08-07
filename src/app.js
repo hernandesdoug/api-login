fb565200b3034a8a90402c21c79230d13946d067
Douglas Hernandes <douglashernandes@icloud.com>
2026-08-06 21:47:41 -0300

    adicionar helmet como middleware global de endpoint

--- src/app.js
+++ src/app.js
@@ -1,25 +1,26 @@
-const dotenv = require("dotenv");
-const path = require("path");
+const dotenv = require('dotenv');
+const path = require('path');
 
-dotenv.config({ path: path.join(__dirname, "..", ".env") });
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
-const express = require("express");
-const cors = require("cors");
-const usersRoutes = require("./routes/usersRoutes");
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("./config/swagger");
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const usersRoutes = require('./routes/users.routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger');
+const errorMiddleware = require('./middleware/error.middleware');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
 
 app.use(usersRoutes);
 
-app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerSpec)
-);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use(errorMiddleware);
 
 module.exports = app;
