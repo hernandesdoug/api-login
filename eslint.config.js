2e655d71d4a04242235cb08d3b29f10c5c919897
Douglas Hernandes <douglashernandes@icloud.com>
2026-08-06 21:25:04 -0300

    adicionar eslint/prettier para capturar erros no codigo

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+const eslint = require('@eslint/js');
+const prettierConfig = require('eslint-config-prettier');
+const importPlugin = require('eslint-plugin-import');
+const globals = require('globals');
+
+module.exports = [
+  eslint.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      // Enforce import/export syntax
+      'import/no-unresolved': 'error',
+      'import/named': 'error',
+      'import/default': 'error',
+      'import/export': 'error',
+
+      // Prevent usage of variables before they are defined
+      'no-use-before-define': 'error',
+
+      // Enforce consistent quote style (single quotes)
+      quotes: ['error', 'single', { avoidEscape: true }],
+
+      // Require semicolons
+      semi: ['error', 'always'],
+
+      // Disallow unused variables
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  prettierConfig,
+];
